0cd153c4043781951b5d875a3eaf68efbafa891a
Gavin Liu <gavin6liu@gmail.com>
2018-02-23 11:09:48 +0800

    first commit

---
+++ app/src/main/java/cn/gavinliu/android/sample/pdfjs/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.gavinliu.android.sample.pdfjs
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ pdfjs/src/main/java/cn/gavinliu/android/lib/pdfjs/PDFWebView.kt
@@ -0,0 +1,38 @@
+package cn.gavinliu.android.lib.pdfjs
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
+import android.util.AttributeSet
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+
+
+class PDFWebView : WebView {
+
+    private val PDFJS_ASSETS_PATH = "file:///android_asset/pdfjs-1.9.426-dist/web/viewer.html"
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun init() {
+        val settings = settings
+        settings.allowFileAccess = true
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.allowFileAccessFromFileURLs = true
+        settings.allowUniversalAccessFromFileURLs = true
+        webChromeClient = WebChromeClient()
+    }
+
+    fun loadFromFile(pdfFilePath: String) {
+        loadUrl(PDFJS_ASSETS_PATH + "?file=file://" + Uri.encode(pdfFilePath, "UTF-8"))
+    }
+
+    fun loadFromAssets(pdfAssetsPath: String) {
+        loadUrl(PDFJS_ASSETS_PATH + "?file=file:///android_asset/" + Uri.encode(pdfAssetsPath, "UTF-8"))
+    }
+}
